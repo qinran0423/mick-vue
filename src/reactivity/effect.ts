ff960b1151d15f22103708d7be11f34ce3067776
qinran0423 <1156107187@qq.com>
2021-09-09 23:09:20 +0800

    effect & scheduler & stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,20 +1,32 @@
 let activeEffect
-export function effect(fn) {
-
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const scheduler = options.scheduler
+  const _effect = new ReactiveEffect(fn, scheduler)
 
   _effect.run()
-
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
 }
 class ReactiveEffect {
   private _fn: any
-  constructor(fn) {
+  deps = [];
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
+  stop() {
+    cleanupEffect(this)
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  });
 }
 
 const targetMap = new Map()
@@ -38,19 +50,27 @@ export function track(target, key) {
 
 export function trackEffects(dep) {
   dep.add(activeEffect)
+  activeEffect && activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
   triggerEffects(dep)
-
-
 }
 
 
 export function triggerEffects(dep) {
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
+}
+
+
+export function stop(runner) {
+  runner.effect.stop()
 }
--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from '../effect';
+import { effect, stop } from '../effect';
 import { reactive } from '../reactive';
 
 describe('effect', () => {
@@ -18,5 +18,70 @@ describe('effect', () => {
     // update
     user.age++
     expect(nextAge).toBe(12)
+  })
+
+
+  it('should return runner when call effect', () => {
+    // effect(fn) -> function(runner) -> fn -> return
+    let foo = 10;
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe("foo")
+  })
+
+  it('description', () => {
+    // 1. 通过effect的第二个参数给定的一个scheduler的fn
+    // 2. effect第一次执行的时候 还会执行fn 
+    // 3. 当 响应式对象 set update不会执行fn 而是执行 scheduler
+    // 4. 如果说当执行runner 的时候  会再次执行fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run 
+    run()
+    // should have run 
+    expect(dummy).toBe(2)
+  })
+
+  it('stop', () => {
+    let dummy;
+    const obj = reactive({
+      prop: 1
+    })
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    // stopped effect should still be manually callable
+    runner()
+    expect(dummy).toBe(3)
   })
 })
