1beeb2e7712e6ac1aed7c3c2d4678ca0c989ad9c
qinran0423 <1156107187@qq.com>
2021-10-03 23:03:20 +0800

    processElement

--- example/helloworld/app.js
+++ example/helloworld/app.js
@@ -2,7 +2,12 @@
 import { h } from '../../lib/mick-vue.esm.js'
 export const App = {
   render() {
-    return h('div', 'hi,' + this.msg)
+    return h('div', {
+      id: 'root',
+      class: ['red', 'hard']
+    },
+      [h('p', { class: 'red' }, 'hi'), h('p', { class: 'blue' }, 'mick-vue')]
+    )
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -10,8 +11,41 @@ function patch(vnode, container) {
   // 处理组件
   // TODO判断是不是一个element类型
   // 如果是element
-  // processElement()
-  processComponent(vnode, container)
+  console.log(vnode.type);
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+
+  const { type, props, children } = vnode
+
+  const el = document.createElement(type)
+
+  // children => string, array
+  if (typeof children === 'string') {
+    el.textContent = children
+    for (const key in props) {
+      const val = props[key]
+      el.setAttribute(key, val)
+    }
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, container)
+  }
+
+  container.append(el)
+}
+
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => patch(v, container))
 }
 
 function processComponent(vnode, container) {
@@ -34,3 +68,6 @@ function setupRenderEffect(instance: any, container: any) {
 
   patch(subTree, container)
 }
+
+
+
